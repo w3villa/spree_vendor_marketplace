c43b391bc730dbadcf3e1ff4b2e64c8d878136a0
manish rai <manish.rai@w3villa.com>
2017-08-08 12:28:46 +0530

    rename store_user model name to store_spree_user

--- app/models/merchant/store_spree_user.rb
+++ app/models/merchant/store_spree_user.rb
@@ -1,5 +1,5 @@
 module Merchant
-	class StoreUser < ActiveRecord::Base
+	class StoreSpreeUser < ActiveRecord::Base
 		belongs_to :store, foreign_key: :store_id, class_name: 'Merchant::Store'
 		belongs_to :spree_user, foreign_key: :spree_user_id, class_name: 'Spree::User'
 		
@@ -7,3 +7,4 @@ module Merchant
 	  belongs_to :spree_sell_privilege
 	end
 end
+
