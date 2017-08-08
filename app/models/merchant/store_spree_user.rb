3139dc7db1e748a88098560a77360350f91e3b8a
manish rai <manish.rai@w3villa.com>
2017-08-08 12:50:06 +0530

    chnage table name back to spree_store_user

--- app/models/merchant/store_spree_user.rb
+++ app/models/merchant/store_spree_user.rb
@@ -1,6 +1,6 @@
 module Merchant
-	class StoreUser < ActiveRecord::Base
-		self.table_name = "store_spree_users" 
+	class StoreSpreeUser < ActiveRecord::Base
+		# self.table_name = "store_spree_users" 
 		belongs_to :store, foreign_key: :store_id, class_name: 'Merchant::Store'
 		belongs_to :spree_user, foreign_key: :spree_user_id, class_name: 'Spree::User'
 		
