ff611492bc16adbfeeb9d6b5740d4330309f666b
manish rai <manish.rai@w3villa.com>
2017-08-08 12:54:49 +0530

    change class name in user decorator

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,7 +1,7 @@
 if Spree.user_class
 	Spree.user_class.class_eval do
 
-		has_many :store_spree_users, foreign_key: :spree_user_id, class_name: 'Merchant::StoreUser'
+		has_many :store_spree_users, foreign_key: :spree_user_id, class_name: 'Merchant::StoreSpreeUser'
 	  has_many :stores, through: :store_spree_users, class_name: 'Merchant::Store'
 	end
 end
